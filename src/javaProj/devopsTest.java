a976f0f6a2d2af7f82315a6a3a70ad99fd024858
purushotham <purushotham.reddy@cgi.com>
2022-02-25 21:15:23 +0530

    first java project commit

---
+++ src/javaProj/devopsTest.java
@@ -0,0 +1,11 @@
+package javaProj;
+
+public class devopsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java Project.");
+
+	}
+
+}
